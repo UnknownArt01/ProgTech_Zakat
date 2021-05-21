4c1a14dea1fc8fd370c8f52304b11be77027a5a9
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-05-21 14:15:37 +0700

    second Init

---
+++ app/src/main/java/com/nadi/zakat/AddBarang.java
@@ -0,0 +1,14 @@
+package com.nadi.zakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBarang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_barang);
+    }
+}
